0a9250ffea18670b367a15ca605f9061e827edc2
Cat4848 <catalinrol@gmail.com>
2024-06-11 18:51:01 +0300

    chore: eslint config

---
+++ server/eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest"
+      }
+    },
+    ignores: ["**/*.js"],
+    rules: {
+      "@/no-console": "error"
+    }
+  }
+);
