7431ec33217670eff90a00982861efe0ef681578
Cat4848 <catalinrol@gmail.com>
2024-06-12 20:23:58 +0300

    chore: deployment config

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import session from "express-session";
 import authRouter from "./src/routes/auth/index.js";
 import passport from "passport";
 
@@ -8,9 +9,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || [],
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+};
+
+if (app.get("env") === "production") {
+  sessionOptions.cookie.secure = true;
+}
+
+app.use(session(sessionOptions));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", authRouter);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 4000);
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,7 +3,10 @@ import express from "express";
 const authRouter = express();
 
 authRouter.get("/", (req, res) => {
-  res.json({ message: "Hello Everyone!" });
+  res.json({
+    message: "Hello Everyone!",
+    sessionSecret: process.env.SESSION_SECRET
+  });
 });
 
 authRouter.post("/", (req, res) => {});
