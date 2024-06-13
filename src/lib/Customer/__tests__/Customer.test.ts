7af8d7b3ff6f62ce64e2388ae3d4cf244c0e2491
Cat4848 <catalinrol@gmail.com>
2024-06-13 16:40:46 +0300

    feat: customer class init

---
+++ src/lib/Customer/__tests__/Customer.test.ts
@@ -0,0 +1,6 @@
+import Customer from "../Customer";
+
+test("if customer instance created successfully", () => {
+  const customer = new Customer();
+  expect(customer).toHaveProperty("first_name");
+});
