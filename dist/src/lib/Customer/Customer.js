8fdc81f1ac2d3ce35755e844cfe753e94911969e
Cat4848 <catalinrol@gmail.com>
2024-06-13 17:00:36 +0300

    feat: customer class added properties

--- dist/src/lib/Customer/Customer.js
+++ dist/src/lib/Customer/Customer.js
@@ -1,2 +1,14 @@
 export default class Customer {
+    customer_id;
+    officer_id;
+    first_name;
+    last_name;
+    email;
+    constructor(officer_id, first_name, last_name, email) {
+        this.customer_id = 1;
+        this.officer_id = officer_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
 }
--- src/lib/Customer/Customer.ts
+++ src/lib/Customer/Customer.ts
@@ -1,3 +1,20 @@
-export default class Customer{
-  
+export default class Customer {
+  private customer_id: number;
+  private officer_id: number;
+  private first_name: string;
+  private last_name: string;
+  private email: string;
+
+  constructor(
+    officer_id: number,
+    first_name: string,
+    last_name: string,
+    email: string
+  ) {
+    this.customer_id = 1;
+    this.officer_id = officer_id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+  }
 }
