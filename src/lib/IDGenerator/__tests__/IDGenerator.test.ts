6e8bde47b121f8cc98bc89fc57edfce7fd5464bc
Cat4848 <catalinrol@gmail.com>
2024-06-13 17:28:08 +0300

    test: stress test for ID generator

--- src/lib/IDGenerator/__tests__/IDGenerator.test.ts
+++ src/lib/IDGenerator/__tests__/IDGenerator.test.ts
@@ -1,6 +1,6 @@
 import IDGenerator from "../IDGenerator";
 
-test("if random ID is small INT", () => {
+test.each(Array(100).fill(0))("if random ID is small INT", () => {
   const smallIntID = IDGenerator.smallIntRandomID();
   expect(smallIntID).toBeGreaterThanOrEqual(0);
   expect(smallIntID).toBeLessThanOrEqual(32000);
