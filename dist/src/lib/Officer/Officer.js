6e9173cfb589ba028ceac1205177b8332910a64c
Cat4848 <catalinrol@gmail.com>
2024-06-14 18:20:59 +0100

    refactor: officer class

--- dist/src/lib/Officer/Officer.js
+++ dist/src/lib/Officer/Officer.js
@@ -1,10 +1,10 @@
-import IDGenerator from "../IDGenerator/IDGenerator";
 export default class Officer {
-    #officer_id = IDGenerator.smallIntRandomID();
+    #officer_id;
     #first_name;
     #last_name;
     #email;
-    constructor({ first_name, last_name, email }) {
+    constructor({ officer_id, first_name, last_name, email }) {
+        this.#officer_id = officer_id;
         this.#first_name = first_name;
         this.#last_name = last_name;
         this.#email = email;
