61b23f956b8c8563cc68ed9620fe49b46911543f
Cat4848 <catalinrol@gmail.com>
2024-06-14 11:52:44 +0100

    feat: customer class complete

--- src/lib/Customer/Customer.ts
+++ src/lib/Customer/Customer.ts
@@ -9,16 +9,36 @@ interface CustomerProps {
 }
 
 export default class Customer {
-  private customer_id: number = IDGenerator.smallIntRandomID();
-  private officer_id: number;
-  private first_name: string;
-  private last_name: string;
-  private email: string;
+  #customer_id: number = IDGenerator.smallIntRandomID();
+  #officer_id: number;
+  #first_name: string;
+  #last_name: string;
+  #email: string;
 
   constructor({ officer_id, first_name, last_name, email }: CustomerProps) {
-    this.officer_id = officer_id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.email = email;
+    this.#officer_id = officer_id;
+    this.#first_name = first_name;
+    this.#last_name = last_name;
+    this.#email = email;
+  }
+
+  get customer_id() {
+    return this.#customer_id;
+  }
+
+  get officer_id() {
+    return this.#officer_id;
+  }
+
+  get first_name() {
+    return this.#first_name;
+  }
+
+  get last_name() {
+    return this.#last_name;
+  }
+
+  get email() {
+    return this.#email;
   }
 }
