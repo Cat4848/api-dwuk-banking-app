7afaf949908138c94a62c0c737fdc9c5b4684561
Cat4848 <catalinrol@gmail.com>
2024-06-14 18:11:01 +0100

    refactor: customer class

--- dist/src/lib/Customer/Customer.js
+++ dist/src/lib/Customer/Customer.js
@@ -1,11 +1,11 @@
-import IDGenerator from "../IDGenerator/IDGenerator";
 export default class Customer {
-    #customer_id = IDGenerator.smallIntRandomID();
+    #customer_id;
     #officer_id;
     #first_name;
     #last_name;
     #email;
-    constructor({ officer_id, first_name, last_name, email }) {
+    constructor({ customer_id, officer_id, first_name, last_name, email }) {
+        this.#customer_id = customer_id;
         this.#officer_id = officer_id;
         this.#first_name = first_name;
         this.#last_name = last_name;
