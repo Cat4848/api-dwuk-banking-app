35f983a5bc250eadf4be4ca307dbd671dac06d4d
Cat4848 <catalinrol@gmail.com>
2024-06-16 22:15:55 +0100

    chore: changed headers

--- dist/src/routes/helpers/setHeaders.js
+++ dist/src/routes/helpers/setHeaders.js
@@ -1,5 +1,5 @@
 export default function setHeaders(res) {
-    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Origin", "https://herokuapp.com");
     res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.set("Access-Control-Allow-Headers", "Content-Type");
 }
