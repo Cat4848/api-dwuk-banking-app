544b714cc85ca1af346800b9ae8425bfd7fdffe9
Cat4848 <catalinrol@gmail.com>
2024-06-16 22:23:20 +0100

    chore: headers

--- dist/src/routes/helpers/setHeaders.js
+++ dist/src/routes/helpers/setHeaders.js
@@ -1,5 +1,5 @@
 export default function setHeaders(res) {
-    res.set("Access-Control-Allow-Origin", "https://herokuapp.com");
+    res.set("Access-Control-Allow-Origin", "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com");
     res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.set("Access-Control-Allow-Headers", "Content-Type");
 }
--- src/routes/helpers/setHeaders.ts
+++ src/routes/helpers/setHeaders.ts
@@ -1,7 +1,10 @@
 import { Response } from "express";
 
 export default function setHeaders(res: Response) {
-  res.set("Access-Control-Allow-Origin", "https://herokuapp.com");
+  res.set(
+    "Access-Control-Allow-Origin",
+    "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com"
+  );
   res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.set("Access-Control-Allow-Headers", "Content-Type");
 }
