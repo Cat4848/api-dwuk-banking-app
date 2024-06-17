ef8b5c455564956bfb1eb023ed085ee583285ac2
Cat4848 <catalinrol@gmail.com>
2024-06-17 06:08:35 +0100

    chore: headers

--- src/routes/helpers/setHeaders.ts
+++ src/routes/helpers/setHeaders.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 
 export default function setHeaders(res: Response) {
   res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  // res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.set("Access-Control-Allow-Headers", "Content-Type");
 }
