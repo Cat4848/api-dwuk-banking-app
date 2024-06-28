cc3c02e2b52b96d7bb6db4c3d82c4f84d80e04f6
Cat4848 <catalinrol@gmail.com>
2024-06-28 15:16:44 +0100

    chore: removed env.ts file

--- src/database/DatabaseConnection/DatabaseConnection.ts
+++ src/database/DatabaseConnection/DatabaseConnection.ts
@@ -1,11 +1,8 @@
 import mysql from "mysql2/promise";
-import JAWSDB_URL from "../../env";
 
 export default class DatabaseConnection {
   public static async createConnection() {
-    const connection = await mysql.createConnection(
-      process.env.JAWSDB_URL || JAWSDB_URL
-    );
+    const connection = await mysql.createConnection(process.env.JAWSDB_URL);
     return connection;
   }
 }
