23da39253db519ddcfe58fd9cd1b6b467d54e0a0
Cat4848 <catalinrol@gmail.com>
2024-06-28 15:50:22 +0100

    chore: db url from env config

--- src/database/DatabaseConnection/DatabaseConnection.ts
+++ src/database/DatabaseConnection/DatabaseConnection.ts
@@ -1,8 +1,11 @@
 import mysql from "mysql2/promise";
+require("dotenv").config();
 
 export default class DatabaseConnection {
   public static async createConnection() {
-    const connection = await mysql.createConnection(process.env.JAWSDB_URL);
+    const connection = await mysql.createConnection(
+      process.env.JAWSDB_URL || "JAWSDB_URL missing"
+    );
     return connection;
   }
 }
