d4d6e08bafc0a2f020eedfb20f852f7dc0f4290f
Cat4848 <catalinrol@gmail.com>
2024-08-23 08:23:19 +0100

    feat: added mysql pool connection

--- src/database/DatabaseConnection/DatabaseConnection.ts
+++ src/database/DatabaseConnection/DatabaseConnection.ts
@@ -2,10 +2,15 @@ import mysql from "mysql2/promise";
 import "dotenv/config";
 
 export default class DatabaseConnection {
-  public static async createConnection() {
-    const connection = await mysql.createConnection(
-      process.env.JAWSDB_URL || "JAWSDB_URL missing"
-    );
+  private jawsDbUrl = process.env.JAWSDB_URL || "JAWSDB_URL missing";
+
+  public async createConnection() {
+    const connection = await mysql.createConnection(this.jawsDbUrl);
     return connection;
+  }
+
+  public createPool() {
+    const poolConnection = mysql.createPool(this.jawsDbUrl);
+    return poolConnection;
   }
 }
