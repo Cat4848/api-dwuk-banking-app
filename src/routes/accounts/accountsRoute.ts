2c5294afe45cadfeb301c9463a0d2d1c1606451a
Cat4848 <catalinrol@gmail.com>
2024-08-27 17:20:53 +0100

    fix: server error, added console.log statement

--- src/routes/accounts/accountsRoute.ts
+++ src/routes/accounts/accountsRoute.ts
@@ -48,6 +48,7 @@ accountsRouter.get("/:id", async (req, res) => {
 
 accountsRouter.put("/freeze/", async (req, res) => {
   console.log("freezeRoute -> req.body", req.body);
+  console.log("freezeRoute -> req.body.accountIDs", req.body.accountIDs);
   const accountIDs: number[] = JSON.parse(req.body.accountIDs);
   console.log("freezeRoute -> accountIDs", accountIDs);
   try {
