f98d51c5fb5f8949489b78dd297cfb8b86e8eb4d
Cat4848 <catalinrol@gmail.com>
2024-08-29 12:44:08 +0100

    refactor: account status API endpoints

--- src/routes/accounts/accountsRoute.ts
+++ src/routes/accounts/accountsRoute.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import createAccountsDatabase from "../../database/DatabasePersistance/AccountsDatabasePersistance/__tests__/helpers/createAccountsDatabase";
 import setHeaders from "../helpers/setHeaders";
-import createAccountsDatabaseOnPool from "../../database/DatabasePersistance/AccountsDatabasePersistance/__tests__/helpers/createAccountsDatabaseOnPool";
-
+import handlePutAccountStatus from "./helpers/handlePutAccountStatus";
 const accountsRouter = express();
 
 accountsRouter.get("/", async (req, res) => {
@@ -57,22 +56,5 @@ accountsRouter.put("/close", async (req, res) => {
 accountsRouter.put("/freeze", async (req, res) => {
   handlePutAccountStatus(req, res, "freeze");
 });
-
-async function handlePutAccountStatus(
-  req: Request,
-  res: Response,
-  status: "activate" | "close" | "freeze"
-) {
-  const accountIDs: number[] = JSON.parse(req.body.accountIDs);
-  try {
-    const accountsDatabase = createAccountsDatabaseOnPool();
-    const freezeResults = await accountsDatabase[status](accountIDs);
-
-    if (!freezeResults.success) throw freezeResults.error;
-    return res.json(freezeResults.data);
-  } catch (e) {
-    return res.status(404).json(e);
-  }
-}
 
 export default accountsRouter;
